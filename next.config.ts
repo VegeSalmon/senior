6b6bd9c8b4b438f56db500cc7e262603de8a873c
VegeSalmon <vegesalmon@gmail.com>
2025-12-05 23:24:34 +0100

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/senior",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
